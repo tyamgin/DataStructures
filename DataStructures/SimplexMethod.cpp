bc21fc8071965cc7e865e9b858d98bb5d64657f6
tyamgin <tyamgin@mail.ru>
2013-10-04 09:02:07 +0300

    .

---
+++ DataStructures/SimplexMethod.cpp
@@ -0,0 +1,165 @@
+#define TESTING 1
+
+#pragma comment(linker, "/STACK:500000000") 
+#include <functional>
+#include <algorithm> 
+#include <iostream> 
+#include <string.h> 
+#include <stdlib.h> 
+#include <sstream> 
+#include <ctype.h> 
+#include <stdio.h> 
+#include <bitset>
+#include <vector> 
+#include <string> 
+#include <math.h> 
+#include <time.h> 
+#include <queue> 
+#include <stack> 
+#include <list>
+#include <map> 
+#include <set> 
+#define Int long long 
+#define INF 0x3F3F3F3F 
+#define eps 1e-9
+using namespace std;
+
+// http://abc.vvsu.ru/Books/ebooks_iskt/%DD%EB%E5%EA%F2%F0%EE%ED%ED%FB%E5%F3%F7%E5%E1%ED%E8%EA%E8/%C8%F1%F1%EB%E5%E4%EE%E2%E0%ED%E8%E5%20%EE%EF%E5%F0%E0%F6%E8%E9/fmi.asf.ru/vavilov/sm6.htm
+
+struct SimplexMethod {
+	int rows, columns;
+	vector<vector<double> > a;
+	vector<int> basis;
+	vector<double> diff;		// разности
+
+	double &X(int i, int j) {
+		return a[i][j];
+	}
+
+	double &X(int i) {
+		return a[i][columns];
+	}
+
+	bool gauss(vector<vector<double> > &a) {
+		int j;
+		basis.clear();
+		for(int row = 0, col = 0; row < rows && col < columns; col++) {
+			int pos = row;
+			for(j = row; j < rows; j++)
+				if (fabs(a[pos][col]) < fabs(a[j][col]))
+					pos = j;
+			swap(a[row], a[pos]);
+			if (fabs(a[row][col]) < eps)
+				continue;
+			for(j = 0; j < rows; j++) {
+				if (row != j) {
+					double c = a[j][col] / a[row][col];
+					for(int k = 0; k <= columns; k++)
+						a[j][k] -= a[row][k] * c;
+				}
+			}
+			basis.push_back(col);
+			row++;
+		}
+		if (basis.size() < rows)
+			return false;
+		for(int i = 0; i < rows; i++) {
+			double center = a[i][basis[i]];
+			for(int j = 0; j <= columns; j++)
+				a[i][j] /= center;
+		}
+		return true;
+	}
+
+	int getJ() {
+		return max_element(diff.begin(), diff.end()) - diff.begin();
+	}
+
+	int getI(int L) {
+		double min = 1e100;
+		int I = -1;
+		for(int i = 0; i < rows; i++)
+			if (X(basis[i], L) > eps && min > X(basis[i]) / X(basis[i], L))
+				min = X(basis[i]) / X(basis[i], L), I = i;
+		return I;
+	}
+
+	int Do(vector<vector<double> > _a, vector<double> c, vector<double> &result) {		
+		rows = _a.size();
+		columns = _a[0].size() - 1;
+		basis.resize(rows);
+		for(int i = 0; i < rows; i++)
+			basis[i] = i;
+		if (!gauss(_a))
+			return 0;
+		diff.resize(columns);
+		a.assign(columns, vector<double>(columns + 1));
+		for(int i = 0; i < rows; i++)
+			a[basis[i]] = _a[i];
+		for(int j = 0; j < columns; j++) {
+			double sum = 0;
+			for(int i = 0; i < rows; i++)
+				sum += c[basis[i]] * a[basis[i]][j];
+			diff[j] = sum - c[j];
+		}
+
+		while(1) {
+			int L = getJ();
+			if (diff[L] < eps) {
+				result.clear();
+				for(int i = 0; i < columns; i++)
+					result.push_back(X(i));
+				return 1;
+			}
+
+			int whereI = getI(L);
+			if (whereI < 0)
+				return INF;
+			int I = basis[whereI];
+			double teta = X(I) / X(I, L);
+			vector<vector<double> > na = a;
+			vector<double> ndiff = diff;
+			na[L][columns] = teta; // new x
+			for(int j = 0; j < columns; j++)
+				na[L][j] = X(I, j) / X(I, L);
+
+			for(int row = 0; row < rows; row++) {
+				if (row != whereI) {
+					int i = basis[row]; 
+					na[i][columns] = X(i) - teta * X(i, L);
+					for(int j = 0; j < columns; j++)
+						na[i][j] = X(i, j) - X(I, j) / X(I, L) * X(i, L);
+				}
+			}
+			for(int j = 0; j < columns; j++)
+				ndiff[j] = diff[j] - X(I, j) / X(I, L) * diff[L];
+			basis[whereI] = L;
+			a = na;
+			diff = ndiff;
+		}
+	}
+};
+
+#if TESTING
+
+int main()
+{
+	double c[] = {0, 1, -3, 0, 2, 0};
+	double a[3][7] = {{1, 3, -1, 0, 2, 0,  7},
+			     	  {0, -2, 4, 1, 0, 0,  12},	
+				      {0, -4, 3, 0, 8, 1,  10}};
+	//double a[3][7] = {{1, 1, 1, 1, 1, 1,  7},
+	//		     	  {1, 1, 1, 1, 1, 1,  12},	
+	//			      {0, -4, 3, 0, 8, 1,  10}};
+	int basis[] = {0, 3, 5};
+
+	vector<vector<double> > A(3);
+	for(int i = 0; i < 3; i++)
+		A[i].insert(A[i].begin(), a[i], a[i] + 7);
+	SimplexMethod sm;
+	vector<double> res;
+	cout << sm.Do(A, vector<double>(c, c + 7), res) << endl;
+	return 0;
+}
+
+#endif;
